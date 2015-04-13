ca0d6b0c95f0495602994746c8eb1abd8b09214c
Tushar Makkar <tusharmakkar08@gmail.com>
2015-04-13 18:15:37 +0530

    Main function skeleton added

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Bloom Filter Code in python
+Author: Tushar Makkar <tusharmakkar08[at]gmail.com>
+'''
+
+if __name__ == "__main__":
+    '''
+    Main function where everything is called
+    '''
